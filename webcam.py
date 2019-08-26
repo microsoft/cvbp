95d639e1a0920b39e2093c6347b231433657d467
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2019-08-26 13:21:38 +0800

    Update dependencies and webcam for image classification

--- webcam.py
+++ webcam.py
@@ -11,32 +11,88 @@
 # From the Microsoft Best Practices Suite: Computer Vision
 # https://github.com/microsoft/ComputerVision
 
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
 import sys
-import os
-import urllib.request
-import argparse
+import torch
 
-from mlhub.pkg import is_url
-from mlhub.utils import get_cmd_cwd
-
-import fastai
-from fastai.vision import models, open_image, load_learner, untar_data
-
-from ipywebrtc import CameraStream, ImageRecorder
+from fastai.vision import models, Image
+from matplotlib.animation import FuncAnimation
 
 # Until this is pip installable we use a local copy!
-
-from utils_cv.common.data import data_path
-from utils_cv.common.gpu import which_processor
 from utils_cv.classification.data import imagenet_labels
 from utils_cv.classification.model import IMAGENET_IM_SIZE, model_to_learner
 
+
+# ----------------------------------------------------------------------
+# Helper
 # ----------------------------------------------------------------------
 
+TEXT_COLOR = (0, 255, 0)  # Green
+LINE_WIDTH = 2
+TEXT_FONT = cv.FONT_HERSHEY_SIMPLEX
+TEXT_SIZE = 0.75
+
+
+def get_camera(num=0):
+    """Get the camera.
+
+    :param num:  The camera number.  By default the first camera will be returned.
+    :return: The camera used by OpenCV.
+    """
+    capture = cv.VideoCapture(num)
+    if not capture.isOpened():
+        print('Unable to load camera!')
+        sys.exit(1)
+
+    return capture
+
+
+def cv2torch(im_cv):
+    """Convert image from OpenCV format to PyTorch tensor.
+
+    See https://forums.fast.ai/t/prediction-on-video-input-file/41029/5
+
+    :param im_cv: Image of OpenCV format.
+    :return: Image of PyTorch tensor.
+    """
+    return torch.tensor(np.ascontiguousarray(np.flip(im_cv, 2)).transpose(2, 0, 1)).float() / 255
+
+
+def put_text(im_cv, text):
+    """Put text on the top-middle of image.
+    """
+
+    (text_width, text_height), _ = cv.getTextSize(text, TEXT_FONT, TEXT_SIZE, LINE_WIDTH)
+    _, img_width, _ = im_cv.shape
+
+    x = int((img_width - text_width) / 2)
+    y = text_height + 5
+    cv.putText(im_cv, text, (x, y), TEXT_FONT, TEXT_SIZE, TEXT_COLOR, LINE_WIDTH)
+
+
+def classify_frame(capture, learner, label):
+    """Use the learner to predict the class label.
+    """
+    _, frame = capture.read()  # Capture frame-by-frame
+    im = Image(cv2torch(frame))
+    _, ind, prob = learner.predict(im)
+    text = f"{label[ind]} ({prob[ind]:.2f})"
+    put_text(frame, text)
+    return cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+
+# ----------------------------------------------------------------------
+# Prepare model
+# ----------------------------------------------------------------------
+
+# Load model labels
 labels = imagenet_labels()
 
-# models.xresnet50
-#learn = model_to_learner(models.resnet18(pretrained=True), IMAGENET_IM_SIZE)
+# Load ResNet model
+# * https://download.pytorch.org/models/resnet18-5c106cde.pth -> ~/.cache/torch/checkpoints/resnet18-5c106cde.pth
+learn = model_to_learner(models.resnet18(pretrained=True), IMAGENET_IM_SIZE)
 #learn = model_to_learner(models.resnet152(pretrained=True), IMAGENET_IM_SIZE)
 #learn = model_to_learner(models.xresnet152(pretrained=True), IMAGENET_IM_SIZE)
 
@@ -46,39 +102,30 @@ labels = imagenet_labels()
 #model = untar_data("resnet18-5c106cde.pth")
 #learn = load_learner(model)
 
-# Webcam
-w_cam = CameraStream(
-    constraints={
-        'facing_mode': 'user',
-        'audio': False,
-        'video': { 'width': IMAGENET_IM_SIZE, 'height': IMAGENET_IM_SIZE }
-    },
-    layout=Layout(width=f'{IMAGENET_IM_SIZE}px')
-)
-# Image recorder for taking a snapshot
-w_imrecorder = ImageRecorder(stream=w_cam, layout=Layout(padding='0 0 0 50px'))
-# Label widget to show our classification results
-w_label = Label(layout=Layout(padding='0 0 0 50px'))
+# ----------------------------------------------------------------------
+# Prepare webcam and plot
+# ----------------------------------------------------------------------
 
-def classify_frame(_):
-    """ Classify an image snapshot by using a pretrained model
-    """
-    # Once capturing started, remove the capture widget since we don't need it anymore
-    if w_imrecorder.layout.display != 'none':
-        w_imrecorder.layout.display = 'none'
-        
-    try:
-        im = open_image(io.BytesIO(w_imrecorder.image.value), convert_mode='RGB')
-        _, ind, prob = learn.predict(im)
-        # Show result label and confidence
-        w_label.value = f"{labels[ind]} ({prob[ind]:.2f})"
-    except OSError:
-        # If im_recorder doesn't have valid image data, skip it. 
-        pass
-    
-    # Taking the next snapshot programmatically
-    w_imrecorder.recording = True
+# Open webcam
+camera = get_camera()
 
-# Register classify_frame as a callback. Will be called whenever image.value changes. 
-w_imrecorder.image.observe(classify_frame, 'value')
+# Turn off axis in plot window
+plt.axis('off')
 
+# ----------------------------------------------------------------------
+# Image classification on webcam
+# ----------------------------------------------------------------------
+
+print("\nPlease close the window (Ctrl-w) to quit.")
+
+im = plt.gca().imshow(classify_frame(camera, learn, labels))
+video = FuncAnimation(
+    plt.gcf(),
+    lambda i: im.set_data(classify_frame(camera, learn, labels)),  # Update plot window with new camera frame
+    interval=100)
+
+plt.show()
+
+# When everything is done, release the capture
+
+camera.release()
